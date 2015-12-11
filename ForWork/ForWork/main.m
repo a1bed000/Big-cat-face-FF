4fd21ca4d97754ed47ecca6c8ce8e084788a5680
sysy <a1bed000@gmail.com>
2015-12-11 13:26:23 +0800

    Project Begin

---
+++ ForWork/ForWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ForWork
+//
+//  Created by 盛愉 on 15/12/11.
+//  Copyright © 2015年 盛愉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
