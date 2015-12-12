97760bf06e6fa92cac72d1997f1065ab3b2979ed
sysy <a1bed000@gmail.com>
2015-12-12 15:20:57 +0800

    add ForWorkProject

---
+++ ForWork/ForWork/General/Views/LaunchViewController.h
@@ -0,0 +1,13 @@
+//
+//  LaunchViewController.h
+//  ForWork
+//
+//  Created by 盛愉 on 15/12/12.
+//  Copyright © 2015年 盛愉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface LaunchViewController : ViewController
+
+@end
---
+++ ForWork/ForWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ForWork
+//
+//  Created by 盛愉 on 15/12/12.
+//  Copyright © 2015年 盛愉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
